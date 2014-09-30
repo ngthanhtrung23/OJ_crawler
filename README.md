31397f8d82024ff3aa6217878f0e33e024bd2cee
Nguyen Thanh Trung <ngthanhtrung23@gmail.com>
2014-09-30 19:08:04 +0800

    Update documentation. Improvement: Timus crawler now accept 2 character code as argument

--- README.md
+++ README.md
@@ -14,5 +14,12 @@ Currently supports following Online Judges:
 
 - [VOJ](http://vn.spoj.com/):
 
+    - To run: python crawler/VOJ.py <username> <password>
+
+- [SPOJ](http://www.spoj.com/):
+
     - To run: python crawler/SPOJ.py <username> <password>
 
+- [Timus](http://acm.timus.ru/):
+
+    - To run: python crawler/timus.py <userid> <password> <2 character code of userid>
--- crawler/timus.py
+++ crawler/timus.py
@@ -1,6 +1,6 @@
 import os
+import sys
 from generic_crawler import GenericCrawler
-from VOJ import main
 
 OUTPUT_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../code/Timus')
 BASE_URL = 'http://acm.timus.ru/'
@@ -19,6 +19,7 @@ class TimusCrawler(GenericCrawler):
             '.*<a href="getsubmit\.aspx/(?P<id>[\da-z\.]+)"'
         )
         self.password = ''
+        self.user_code = ''
 
     def login(self, _, password):
         self.password = password
@@ -28,11 +29,37 @@ class TimusCrawler(GenericCrawler):
         code_url = self.code_url.format(id=submission_id)
         request = self.session.post(code_url, data={
             'Action': 'getsubmit',
-            'JudgeId': username + 'PC',
+            'JudgeId': username + self.user_code,
             'PASSWORD': self.password
         })
         self.store_code(output_dir, problem_code, submission_id[submission_id.find('.')+1:], request.text)
         print submission_id
+
+
+def main(crawler, output_dir):
+    if len(sys.argv) >= 4:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        crawler.user_code = sys.argv[3]
+    else:
+        username = raw_input('Your username: ')
+        password = raw_input('Your password: ')
+        crawler.user_code = raw_input('Your 2 character user code: ')
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    crawler.login(username, password)
+
+    problems = crawler.get_solved_problems(username)
+    print '{} has solved {} problems'.format(username, len(problems))
+
+    for problem in problems:
+        for extension in ['java', 'cpp', 'py', 'pas']:
+            if os.path.isfile(os.path.join(output_dir, problem + '.' + extension)):
+                break
+        else:
+            crawler.download_solution(output_dir, username, problem)
 
 if __name__ == '__main__':
     crawler = TimusCrawler()
