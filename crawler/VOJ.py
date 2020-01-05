c821ced53c3c628640277dfe2cea3713e67556d2
Le Duy Thuc <leduykhongngu@gmail.com>
2020-01-05 15:54:06 +0700

    Add counting accepted code

--- crawler/VOJ.py
+++ crawler/VOJ.py
@@ -10,31 +10,36 @@ BASE_URL = 'https://vn.spoj.com/'
 all_submitted_code = 0
 accepted_code_only = 1
 def main(crawler, output_dir):
+    
+    type_get = all_submitted_code
+    if sys.argv[-1] == "AC_only":
+        type_get = accepted_code_only
+
     if len(sys.argv) >= 3:
         username = sys.argv[1]
         password = sys.argv[2]
-        if len(sys.argv) >= 4:
-            output_dir = sys.argv[3]
     else:
         username = input('Your username: ')
         password = input('Your password: ')
 
     crawler._login(username, password)
+    output_dir = os.path.join(output_dir, username)
 
-    output_dir = os.path.join(output_dir, username, 'VOJ')
+    if type_get == accepted_code_only:
+        output_dir = os.path.join(output_dir, 'accepted_code')
+
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
     
-    type_get = all_submitted_code
-    if sys.argv[-1] == "AC_only":
-        type_get = accepted_code_only
     
     solved_list = crawler.get_solved_list(username)
-
+    submission_count = solved_list.count('\n') 
+    print("You have", submission_count, "submissions")
+    accepted_submission_count = 0
     
     #|   ID    |        DATE         |  PROBLEM  |  RESULT   | TIME  |  MEM   | LNG |
     csv_reader = csv.reader(StringIO(solved_list), delimiter = '|')
-    submission_count = solved_list.count('\n') 
+    
     for i in tqdm(range(submission_count)):
         row = next(csv_reader)[1 : -1]
         if (row == None):
@@ -45,11 +50,12 @@ def main(crawler, output_dir):
             if re.match('(AC|100)', result) == None:
                 continue
             file_name = row[2]
+            accepted_submission_count += 1
         else:
             #full information
             file_name = "{2}[{0}][RESULT_{3}][TIME_{4}][MEM_{5}][{1}]".format(row[0], row[1].replace(' ', '_').replace(':','-'), row[2], row[3], row[4], row[5], row[5])
         crawler.download_solution(os.path.join(output_dir, file_name), username, row[0], row[6])
-
+    print("You have", accepted_submission_count, "accepted submissions")
 if __name__ == '__main__':
     crawler = SPOJCrawler(BASE_URL,
                 BASE_URL + 'files/src/save/{id}')
